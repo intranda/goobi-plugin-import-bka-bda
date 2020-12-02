246916663e6bed7f2867ca146bb983c6041a83c8
Robert Sehr <robert.sehr@intranda.com>
2020-12-02 15:06:04 +0100

    import metadata

--- goobi-plugin-import-bka/src/main/java/de/intranda/goobi/plugins/BkaBdaImportPlugin.java
+++ goobi-plugin-import-bka/src/main/java/de/intranda/goobi/plugins/BkaBdaImportPlugin.java
@@ -10,7 +10,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.apache.commons.io.input.BOMInputStream;
+import org.apache.commons.lang.StringUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
@@ -18,6 +24,8 @@ import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.goobi.production.cli.helper.StringPair;
+import org.goobi.production.enums.ImportReturnValue;
 import org.goobi.production.enums.ImportType;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.importer.DocstructElement;
@@ -26,21 +34,29 @@ import org.goobi.production.importer.Record;
 import org.goobi.production.plugin.interfaces.IImportPluginVersion2;
 import org.goobi.production.properties.ImportProperty;
 
+import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.forms.MassImportForm;
 import de.sub.goobi.helper.exceptions.ImportPluginException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+import ugh.dl.DigitalDocument;
+import ugh.dl.DocStruct;
+import ugh.dl.DocStructType;
 import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.dl.MetadataType;
 import ugh.dl.Prefs;
+import ugh.exceptions.UGHException;
+import ugh.fileformats.mets.MetsMods;
 
 @PluginImplementation
 @Log4j2
 public class BkaBdaImportPlugin implements IImportPluginVersion2 {
 
     @Getter
-    private String title = "intranda-import-bka-bda";
+    private String title = "intranda_import_bka_bda";
     @Getter
     private PluginType type = PluginType.Import;
 
@@ -64,6 +80,26 @@ public class BkaBdaImportPlugin implements IImportPluginVersion2 {
     @Setter
     private File file;
 
+    @Setter
+    private String workflowTitle;
+
+    private int rowHeader;
+    private int rowDataStart;
+    private int rowDataEnd;
+    private String publicationType;
+    private String imageType;
+
+    private String imageFolderPath;
+    private String imageFolderHeaderName;
+
+    private boolean runAsGoobiScript;
+
+    private List<StringPair> mainMetadataList;
+    private List<StringPair> imageMetadataList;
+
+    private String processTitleColumn;
+    private String collection;
+
     public BkaBdaImportPlugin() {
         importTypes = new ArrayList<>();
         importTypes.add(ImportType.FILE);
@@ -76,21 +112,162 @@ public class BkaBdaImportPlugin implements IImportPluginVersion2 {
 
     @Override
     public List<ImportObject> generateFiles(List<Record> records) {
+        if (StringUtils.isBlank(workflowTitle)) {
+            workflowTitle = form.getTemplate().getTitel();
+        }
+        readConfig();
+
+        DocStructType physicalType = prefs.getDocStrctTypeByName("BoundBook");
+        DocStructType imageDocstructType = prefs.getDocStrctTypeByName(imageType);
+        DocStructType pageType = prefs.getDocStrctTypeByName("page");
+
+        DocStructType logicalType = prefs.getDocStrctTypeByName(publicationType);
+        MetadataType pathimagefilesType = prefs.getMetadataTypeByName("pathimagefiles");
+
         List<ImportObject> answer = new ArrayList<>();
         for (Record record : records) {
-            String title = record.getId();
+            ImportObject io = new ImportObject();
+
+            String title = record.getId().replaceAll("\\W", "_");
 
             List<Map<?, ?>> data = (List<Map<?, ?>>) record.getObject();
             Map<String, Integer> headerMap = (Map<String, Integer>) data.get(0);
-            List<Map<?, ?>> rows =   data.subList(1, data.size());
+            List<Map<?, ?>> rows = data.subList(1, data.size());
 
-            System.out.println("process: " + title);
-            System.out.println(headerMap);
-            System.out.println(rows);
+            // create new mets file
+            try {
+                Fileformat fileformat = new MetsMods(prefs);
+                DigitalDocument dd = new DigitalDocument();
+                fileformat.setDigitalDocument(dd);
 
+                DocStruct physical = dd.createDocStruct(physicalType);
+                dd.setPhysicalDocStruct(physical);
+
+                // imagepath
+                Metadata newmd = new Metadata(pathimagefilesType);
+                newmd.setValue("/images/");
+                physical.addMetadata(newmd);
+
+                DocStruct logical = dd.createDocStruct(logicalType);
+                dd.setLogicalDocStruct(logical);
+                // parse main metadata
+                Map<Integer, String> firstRow = (Map<Integer, String>) rows.get(0);
+                for (StringPair sp : mainMetadataList) {
+                    String rulesetName = sp.getOne();
+                    String columnName = sp.getTwo();
+                    String metadataValue = firstRow.get(headerMap.get(columnName));
+                    if (StringUtils.isNotBlank(metadataValue)) {
+                        MetadataType type = prefs.getMetadataTypeByName(rulesetName);
+                        Metadata md = new Metadata(type);
+                        md.setValue(metadataValue);
+                        logical.addMetadata(md);
+                    }
+                }
+                int currentPhysicalOrder = 0;
+
+                for (Map<?, ?> rawRow : rows) {
+                    // create page element for each image
+                    Map<Integer, String> row = (Map<Integer, String>) rawRow;
+
+                    DocStruct page = dd.createDocStruct(pageType);
+                    page.setImageName(row.get(headerMap.get(imageFolderHeaderName)));
+
+                    MetadataType mdt = prefs.getMetadataTypeByName("physPageNumber");
+                    Metadata mdTemp = new Metadata(mdt);
+                    mdTemp.setValue(String.valueOf(++currentPhysicalOrder));
+                    page.addMetadata(mdTemp);
+
+                    // logical page no
+                    mdt = prefs.getMetadataTypeByName("logicalPageNumber");
+                    mdTemp = new Metadata(mdt);
+                    mdTemp.setValue("uncounted");
+
+                    page.addMetadata(mdTemp);
+                    physical.addChild(page);
+                    logical.addReferenceTo(page, "logical_physical");
+
+                    // create logical element for each image
+                    DocStruct ds = dd.createDocStruct(imageDocstructType);
+                    logical.addChild(ds);
+                    ds.addReferenceTo(page, "logical_physical");
+
+                    // parse image metadata
+
+                    for (StringPair sp : imageMetadataList) {
+                        String rulesetName = sp.getOne();
+                        String columnName = sp.getTwo();
+                        String metadataValue = row.get(headerMap.get(columnName));
+                        if (StringUtils.isNotBlank(metadataValue)) {
+                            MetadataType type = prefs.getMetadataTypeByName(rulesetName);
+                            Metadata md = new Metadata(type);
+                            md.setValue(metadataValue);
+                            ds.addMetadata(md);
+                        }
+                    }
+                }
+                String fileName = getImportFolder() + File.separator + title + ".xml";
+                io.setProcessTitle(title);
+                io.setMetsFilename(fileName);
+                fileformat.write(fileName);
+                io.setImportReturnValue(ImportReturnValue.ExportFinished);
+            } catch (UGHException e) {
+                log.error(e);
+                io.setImportReturnValue(ImportReturnValue.WriteError);
+            }
+
+            // TODO copy images to folder
+
+            io.setProcessTitle(title);
+            answer.add(io);
         }
 
         return answer;
+    }
+
+    private void readConfig() {
+        XMLConfiguration xmlConfig = ConfigPlugins.getPluginConfig(title);
+        xmlConfig.setExpressionEngine(new XPathExpressionEngine());
+        xmlConfig.setReloadingStrategy(new FileChangedReloadingStrategy());
+
+        SubnodeConfiguration myconfig = null;
+        try {
+            myconfig = xmlConfig.configurationAt("//config[./template = '" + workflowTitle + "']");
+        } catch (IllegalArgumentException e) {
+            myconfig = xmlConfig.configurationAt("//config[./template = '*']");
+        }
+
+        if (myconfig != null) {
+            rowHeader = myconfig.getInt("/rowHeader", 1);
+            rowDataStart = myconfig.getInt("/rowDataStart", 2);
+            rowDataEnd = myconfig.getInt("/rowDataEnd", 20000);
+            publicationType = myconfig.getString("/publicationType", "Monograph");
+            imageType = myconfig.getString("/imageType", "Picture");
+
+            imageFolderPath = myconfig.getString("/imageFolderPath", null);
+            imageFolderHeaderName = myconfig.getString("/imageFolderHeaderName", null);
+
+            runAsGoobiScript = myconfig.getBoolean("/runAsGoobiScript", true);
+
+            collection = myconfig.getString("/collection", null);
+            processTitleColumn = myconfig.getString("/processTitleColumn", null);
+
+            mainMetadataList = new ArrayList<>();
+            List<HierarchicalConfiguration> mml = myconfig.configurationsAt("/mainMetadata");
+            for (HierarchicalConfiguration md : mml) {
+                String rulesetName = md.getString("@rulesetName");
+                String headerName = md.getString("@columnName", null);
+                mainMetadataList.add(new StringPair(rulesetName, headerName));
+            }
+
+            imageMetadataList = new ArrayList<>();
+            mml = myconfig.configurationsAt("/imageMetadata");
+            for (HierarchicalConfiguration md : mml) {
+                String rulesetName = md.getString("@rulesetName");
+                String headerName = md.getString("@columnName", null);
+                imageMetadataList.add(new StringPair(rulesetName, headerName));
+            }
+
+        }
     }
 
     @Override
@@ -121,6 +298,10 @@ public class BkaBdaImportPlugin implements IImportPluginVersion2 {
 
     @Override
     public List<Record> generateRecordsFromFile() {
+        if (StringUtils.isBlank(workflowTitle)) {
+            workflowTitle = form.getTemplate().getTitel();
+        }
+        readConfig();
 
         List<Record> recordList = new ArrayList<>();
         Map<String, Integer> headerOrder = new HashMap<>();
@@ -134,9 +315,6 @@ public class BkaBdaImportPlugin implements IImportPluginVersion2 {
             Iterator<Row> rowIterator = sheet.rowIterator();
 
             // get header and data row number from config first
-            int rowHeader = 1;
-            int rowDataStart = 1;
-            int rowDataEnd = 10000;
             int rowCounter = 0;
 
             //  find the header row
@@ -201,7 +379,7 @@ public class BkaBdaImportPlugin implements IImportPluginVersion2 {
                     map.put(cn, value);
                 }
                 // id = Objekttitel + FotografIn + Aufnahmejahr
-                String title = map.get(headerOrder.get("Objekttitel")) + "_" + map.get(headerOrder.get("FotografIn")) + "_" + map.get(headerOrder.get("Aufnahmejahr"));
+                String title = map.get(headerOrder.get(processTitleColumn));
 
                 if (processMap.containsKey(title)) {
                     List<Map<?, ?>> rows = processMap.get(title);
@@ -282,7 +460,7 @@ public class BkaBdaImportPlugin implements IImportPluginVersion2 {
 
     @Override
     public boolean isRunnableAsGoobiScript() {
-        return false;
+        return runAsGoobiScript;
     }
 
 }
--- goobi-plugin-import-bka/src/test/java/de/intranda/goobi/plugins/BkaBdaImportPluginTest.java
+++ goobi-plugin-import-bka/src/test/java/de/intranda/goobi/plugins/BkaBdaImportPluginTest.java
@@ -10,15 +10,29 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.easymock.EasyMock;
 import org.goobi.production.enums.ImportType;
+import org.goobi.production.importer.ImportObject;
 import org.goobi.production.importer.Record;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import de.sub.goobi.config.ConfigPlugins;
 import ugh.dl.Prefs;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ ConfigPlugins.class })
+@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*" })
 public class BkaBdaImportPluginTest {
 
     @Rule
@@ -36,6 +50,9 @@ public class BkaBdaImportPluginTest {
             resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
         }
 
+        PowerMock.mockStatic(ConfigPlugins.class);
+        EasyMock.expect(ConfigPlugins.getPluginConfig(EasyMock.anyString())).andReturn(getConfig()).anyTimes();
+        PowerMock.replay(ConfigPlugins.class);
     }
 
     @Test
@@ -43,9 +60,9 @@ public class BkaBdaImportPluginTest {
         BkaBdaImportPlugin plugin = new BkaBdaImportPlugin();
         assertNotNull(plugin);
         assertEquals(ImportType.FILE, plugin.getImportTypes().get(0));
-
         plugin.setImportFolder(tempFolder.getAbsolutePath());
     }
+
 
     @Test
     public void testUploadExcelFile() throws Exception {
@@ -55,7 +72,7 @@ public class BkaBdaImportPluginTest {
         Prefs prefs = new Prefs();
         prefs.loadPrefs(resourcesFolder + "ruleset.xml");
         plugin.setPrefs(prefs);
-
+        plugin.setWorkflowTitle("workflow");
         File excelFile = new File(resourcesFolder + "metadaten.xlsx");
         assertTrue(excelFile.isFile());
         plugin.setFile(excelFile);
@@ -67,10 +84,48 @@ public class BkaBdaImportPluginTest {
         Map<String, Integer> headerMap = (Map<String, Integer>) data.get(0);
         List<Map<?, ?>> rows = data.subList(1, data.size());
 
-        assertEquals("Topographische Aufnahmen_digital_Michael AAAAAA_2015", title);
+        assertEquals("2011323750B. XXXXXX2014", title);
         assertEquals(21, headerMap.size());
         assertEquals(2, rows.size());
-        assertEquals("Topographische Aufnahmen_digital", rows.get(0).get(headerMap.get("Objekttitel")));
+        assertEquals("Schweizertrakt, Schweizerhof mit Hofburgkapelle, Schatzkammer", rows.get(0).get(headerMap.get("Objekttitel")));
+    }
+
+
+
+    @Test
+    public void testImportFiles()  throws Exception {
+        BkaBdaImportPlugin plugin = new BkaBdaImportPlugin();
+        plugin.setImportFolder(tempFolder.getAbsolutePath());
+        plugin.setTestMode(true); // ruleset
+        Prefs prefs = new Prefs();
+        prefs.loadPrefs(resourcesFolder + "ruleset.xml");
+        plugin.setPrefs(prefs);
+        plugin.setWorkflowTitle("workflow");
+        File excelFile = new File(resourcesFolder + "metadaten.xlsx");
+        plugin.setFile(excelFile);
+        List<Record> records = plugin.generateRecordsFromFile();
+
+        List<ImportObject> convertedFiles = plugin.generateFiles(records);
+
+        ImportObject io =  convertedFiles.get(0);
+
+        assertEquals("2011323750B__XXXXXX2014", io.getProcessTitle());
+
+        // TODO open mets file, check metadata
+
+
+    }
+
+    private XMLConfiguration getConfig() {
+        String file = "plugin_intranda_import_bka_bda.xml";
+        XMLConfiguration config = new XMLConfiguration();
+        config.setDelimiterParsingDisabled(true);
+        try {
+            config.load(resourcesFolder + file);
+        } catch (ConfigurationException e) {
+        }
+        config.setReloadingStrategy(new FileChangedReloadingStrategy());
+        return config;
     }
 
 }
